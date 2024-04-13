8327136a6d3c057bed3d5719181562d3386f400a
Henry Wanjiru <haronkuria2015@gmail.com>
2024-04-13 13:27:40 +0300

    add vnv file

--- Properties/urls.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('',views.home, name='homepage')
-]
--- api/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-def getRoutes(request):
-	routes = [
-		{'GET' : '/properties'},
-		{'GET' : '/properties/id'},
-		{'DELETE' : '/properties/id'},
-		    ]
-	return JsonResponse(routes,safe=False)
